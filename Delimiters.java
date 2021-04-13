4969ed179c008b6f4106ee4d4ee99834e1c34b85
Kennethyeon1 <600020446@fjuhsd.org>
2021-04-13 16:37:05 +0000

    finished ap practice free response - Kenneth Yeon

--- Delimiters.java
+++ Delimiters.java
@@ -11,13 +11,66 @@ public class Delimiters
 		closeDel = close;
 	}
 
+//code by kenneth yeon
 	public ArrayList<String> getDelimitersList(String[] tokens)
 	{
-		return null;
+		//Create an ArrayList
+    ArrayList<String> delimeters = new ArrayList<String>();
+    //Loop through the array
+    for (int i = 0; i < tokens.length; i++)
+    {
+      //if current is open 
+      if (tokens[i].equals(openDel))
+      {
+        //add to ArrayList
+        delimeters.add(tokens[i]);
+      }
+      //if current is closed
+      if(tokens[i].equals(closeDel))
+      {
+        //add to ArrayList
+        delimeters.add(tokens[i]);
+      }
+    }
+    //return ArrayList
+    return delimeters;
 	}
 
+//code by Kenneth Yeon
 	public boolean isBalanced(ArrayList<String> delimiters)
 	{ 
-		return false;
-	}
+	  //int variable
+    int counter = 0;
+    //loop through ArrayList
+    for(int i = 0; i < delimiters.size(); i++)
+    {
+      //if open
+      if (delimiters.get(i).equals(openDel))
+      {
+        //add one to counter
+        counter++;
+      }
+      //if closed
+      if (delimiters.get(i).equals(closeDel))
+      {
+        //subtract from counter
+        counter--;
+      }
+      //if variable < 0
+      if (counter < 0)
+      {
+        //return false
+        return false;
+      }
+      //if variable = 0
+    }
+    //if variable = 0
+    if (counter == 0)
+    {
+      //return true
+      return true;
+    }
+    //return false
+    return false;
+}
 }
